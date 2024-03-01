68132df2fea37be9a3912ef402874438f2dfa9c6
leedongkyu <leedong0225@icloud.com>
2024-03-01 13:50:22 +0900

    date-sort

--- src/components/DiaryList.tsx
+++ src/components/DiaryList.tsx
@@ -1,9 +1,50 @@
+import React, { useState } from "react";
 import { info } from "../App";
+
+interface option{
+    value:string,
+    name:string
+}
+
+const sortOptionList = [
+    {value:"latest",name:"최신순"},
+    {value:"oldest",name:"오래된순"}
+    
+];
+
+const ControlMenu = ({value,onChange,optionList}:{value:string,onChange:React.Dispatch<string>,optionList:option[]})=>{
+    return(
+        <select value={value} onChange={(e)=>onChange(e.target.value)}>
+            {optionList.map((it,idx)=><option key={idx} value={it.value}>{it.name}</option>)}
+        </select>)
+}
+
 const DiaryList = ({diaryList}:{diaryList:info[]})=>{
+
+    const [sortType,setSortType] = useState('latest');
+
+    const getProcessedDiaryList = ()=>{
+        // 비교 함수
+        const compare = (a:info,b:info)=> {
+            if (sortType === 'latest') {
+                return parseInt(b.date) - parseInt(a.date);
+            }
+            else{
+                return parseInt(a.date)  - parseInt(b.date);
+            }
+        }
+
+        // 배열의 깊은 복사하는 방법
+        const copyList:info[] = JSON.parse(JSON.stringify(diaryList));
+        const sortedList = copyList.sort(compare);
+        return sortedList;
+         
+    }
 
     return(
         <div>
-            {diaryList.map((it)=>(
+            <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList}/>
+            {getProcessedDiaryList().map((it)=>(
                 <div key={it.id}>
                     {it.content}
                 </div>
@@ -12,5 +53,9 @@ const DiaryList = ({diaryList}:{diaryList:info[]})=>{
 
     )
 }
+
+DiaryList.defaultProps = {
+    DiaryList:[]
+};
 
 export default DiaryList;
